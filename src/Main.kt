cf16a82c93a7b68037b6621084c3544d68cc5472
PokiSan <larin44@inbox.ru>
2024-08-20 20:15:21 +0300

    fix!

--- src/Main.kt
+++ src/Main.kt
@@ -2,11 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     println("Задание 1")
-    val one = arrayNumber(arrayOf(1,5,6,2,8))
+    val one = getAverage(arrayOf(1,5,6,2,8))
     println("Среднее значение - $one")
-    val two = arrayNumber(arrayOf(5.2,5.6,6.1,2.5,8.8))
+    val two = getAverage(arrayOf(5.2,5.6,6.1,2.5,8.8))
     println("Среднее значение - $two")
-    val three= arrayNumber(arrayOf(2,6.1,1,3,4.7))
+    val three= getAverage(arrayOf(2,6.1,1,3,4.7))
     println("Среднее значение - $three")
 
     println("\nЗадание 2")
@@ -15,7 +15,7 @@ fun main() {
     println(obj.isItIncluded(arrayOf("Привет","Как","2"),2))
 }
 
-fun <T:Number> arrayNumber(array: Array<T>):Double{
+fun <T:Number> getAverage(array: Array<T>):Double{
     val sum = array.sumOf { it.toDouble() }
     return sum/array.size
 }
